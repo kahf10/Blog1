07c1173405fe76f09910b729d55dcaf06b4d8418
Kahf Hussain <kahfnhussain@gmail.com>
2023-01-31 16:42:29 +0100

    Added the comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_134133) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_31_151940) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -21,10 +21,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_134133) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
-    t.string "article"
-    t.string "references"
+    t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  add_foreign_key "comments", "articles"
 end
