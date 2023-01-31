ffb00fdf3692d966b729ac460c743c9c7052977a
Kahf Hussain <kahfnhussain@gmail.com>
2023-01-31 16:19:12 +0100

    Destroy Added

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_13_062753) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_31_134133) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.string "article"
+    t.string "references"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
